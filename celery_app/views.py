4b7bb6d7960f3a4e6b9b9b55fa95c2dc5d8a39aa
Gautam Chaturvedi <gautamggn13@gmail.com>
2025-01-28 23:07:34 +0530

    ADD: Test celery task and sensetive keys added to env

--- celery_app/views.py
+++ celery_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .tasks import sleepy
 
 # Create your views here.
 def index(request):
+    sleepy()
     return HttpResponse("Hello")
--- celery_test/celery.py
+++ celery_test/celery.py
@@ -5,9 +5,9 @@ from django.conf import settings
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_test.settings')
 
-app = Celery('celery_app')
+app = Celery('celery_test')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
